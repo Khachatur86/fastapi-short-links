b8a98c588edd565fcd754f631dd62d42d544498d
Khachatur Sarkisyan <sarkhv@gmail.com>
2025-09-18 23:40:41 +0300

    Add three endpoints for short-url logic

---
+++ url-shortener/schemas/short_url.py
@@ -0,0 +1,14 @@
+from pydantic import (
+    BaseModel,
+)
+
+
+class ShortUrlBase(BaseModel):
+    target_url: str
+    slug: str
+
+
+class ShortUrl(ShortUrlBase):
+    """
+    Модель сокращенной ссылки
+    """
