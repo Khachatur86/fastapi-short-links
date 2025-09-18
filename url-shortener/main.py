d9690405b14c09cb570b3c2e071d97e10c0f6d52
Khachatur Sarkisyan <sarkhv@gmail.com>
2025-09-18 22:13:23 +0300

    Add endpoint read_root

--- url-shortener/main.py
+++ url-shortener/main.py
@@ -1,5 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI(
     title="URL Shortener",
 )
+
+
+@app.get("/")
+def read_root(
+    request: Request,
+    name: str = "World",
+):
+    docs_url = request.url.replace(
+        path="/docs",
+        query="",
+    )
+    return {
+        "message": "Hello World",
+        "docs": str(docs_url),
+    }
