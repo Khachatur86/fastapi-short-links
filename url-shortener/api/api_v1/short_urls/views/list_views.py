52f6a2aa74230670bb3ffc054bdd89a4b6910cfc
Khachatur Sarkisyan <sarkhv@gmail.com>
2025-09-21 22:31:13 +0300

    More nested routes

---
+++ url-shortener/api/api_v1/short_urls/views/list_views.py
@@ -0,0 +1,31 @@
+from fastapi import (
+    APIRouter,
+    status,
+)
+
+from schemas.short_url import (
+    ShortUrl,
+    ShortUrlCreate,
+)
+from api.api_v1.short_urls.crud import storage
+
+router = APIRouter(
+    prefix="/short-urls",
+    tags=["Short URLS"],
+)
+
+
+@router.get("/", response_model=list[ShortUrl])
+def read_short_urls():
+    return storage.get()
+
+
+@router.post(
+    "/",
+    response_model=ShortUrl,
+    status_code=status.HTTP_201_CREATED,
+)
+def create_short_url(
+    short_url_create: ShortUrlCreate,
+):
+    return storage.create(short_url_create)
